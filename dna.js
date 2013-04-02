011b9eea53f91831cce8f5009f6b3863177d2f0b
dpilafian <pilaf@centerkey.com>
2013-04-01 22:06:49 -0700

    For loop hasOwnProperty check
    Makes the code ugly, but it's a necessary evil.

--- dna.js
+++ dna.js
@@ -2,39 +2,40 @@
 // GPLv3/MIT, see: dnajs.org/license.html
 // Copyright (c) 2013 Center Key Software and other contributors
 
-'use strict';
-
 var dna = {};
 
 dna.util = {
    defaults: function(options, defaults) {
+      //Sets each missing field in options to its default value
       if (options)
          for (var field in defaults)
-            if (typeof options[field] == 'undefined')
-               options[field] = defaults[field];
+            if (defaults.hasOwnProperty(field))
+               if (typeof options[field] == 'undefined')
+                  options[field] = defaults[field];
       return options ? options : defaults;
       }
-   }
+   };
 
 dna.core = {
    templates: $('.dna-template'),
    findFieldElems: function(template) {
+      //Returns list of elements whose content is a dna field
       var regex = /^(~~|{{).*(~~|}})$/;  //dna field, example: ~~title~~
       return template.find('*').filter(
          function() { return $(this).text().match(regex); }
          );
       },
    compile: function(template) {
+      //Prepares template to be cloned
       var regex = /~~|{{|}}/g;  //dna base pairs
       dna.core.findFieldElems(template).each(function() {
          var elem = $(this);
          elem.addClass('dna-field-' + elem.text().replace(regex, '')).empty();
          });
-      //debug(template);
       return template.addClass('dna-compiled').data('dna', 0);
       },
-   get: function(templateName) {
-      var template = dna.core.templates.filter('[data-dna-name=' + templateName + ']');
+   getTemplate: function(name) {
+      var template = dna.core.templates.filter('[data-dna-name=' + name + ']');
       if (!template.hasClass('dna-compiled'))
          dna.core.compile(template);
       return template;
@@ -42,24 +43,25 @@ dna.core = {
    };
 
 dna.api = {
-   clone: function(templateName, dataObj, options) {
+   clone: function(name, dataObj, options) {
       options = dna.util.defaults(options, { fade: false });
-      var template = dna.core.get(templateName);
+      var template = dna.core.get(name);
       var elem = template.clone(true, true).removeClass('dna-template dna-compiled');
       template.data('dna', template.data('dna') + 1);
       for (var field in dataObj)
-         elem.find('.dna-field-' + field).html(dataObj[field]);
+         if (dataObj.hasOwnProperty(field))
+            elem.find('.dna-field-' + field).html(dataObj[field]);
       elem.appendTo(template.parent()).addClass('dna-clone');
       if (options.fade)
          elem.hide().fadeIn();
       return elem;
       },
-   empty: function(templateName, options) {
+   empty: function(name, options) {
       options = dna.util.defaults(options, { fade: false });
-      var elems = dna.core.get(templateName).parent().children('.dna-clone');
+      var elems = dna.core.get(name).parent().children('.dna-clone');
       var duration = options.fade ? 'normal' : 0;
       return elems.fadeOut(duration, function() { $(this).remove(); });
-      },
+      }
    };
 
 dna.clone = dna.api.clone;
