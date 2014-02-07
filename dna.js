2341a92ae641c6127f95b12956342c799bfbd3cc
Dem Pilafian <dem@Lightning-2.local>
2014-02-07 01:07:06 -0800

    Release number updated

--- dna.js
+++ dna.js
@@ -1,4 +1,4 @@
-// dna.js Template Cloner ~~ v0.1.3
+// dna.js Template Cloner ~~ v0.1.4
 // MIT/GPLv3 ~~ dnajs.org/license.html
 // Copyright (c) 2013-2014 Center Key Software and other contributors
 
