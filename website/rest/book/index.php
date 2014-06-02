5452cfe8421f30c1c512f008b8720db2a9851345
Dem Pilafian <dem@Lightning-2.local>
2014-06-01 20:46:02 -0700

    Fixes and styling for website

--- website/rest/book/index.php
+++ website/rest/book/index.php
@@ -1,37 +1,21 @@
 <?php
 // dna.js
 
+// Request
 $id = basename($_SERVER["REQUEST_URI"]);
 
-/*
-class Book {
-   public $title;
-   public $author;
-   }
-$books = array();
-
-$book = new book();
-$book->title =  "The DOM";
-$book->author = "Suzy";
-$books[] = $book;
-
-$book = new book();
-$book->title =  "Howdy HTML5";
-$book->author = "Ed";
-$books[] = $book;
-*/
-
+// Resource
 $books = array(
    array("title" => "Go JavaScript", "author" => "Jake"),
    array("title" => "Styling CSS3",  "author" => "Abby"),
    array("title" => "Howdy HTML5",   "author" => "Ed")
    );
-
 if ($id > 0 && $id <= count($books))
    $resource = $books[$id - 1];
 else
-   $resource = array("error" => true, "msg" => "Resource not found");
+   $resource = array("error" => true, "message" => "Resource not found");
 
+// Response
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
 echo json_encode($resource);
