c07a264981dbfc73c78264d343e8d37faf7ebde9
Dem Pilafian <pilaf@centerkey.com>
2015-03-20 11:30:07 -0700

    Version number updated for next release

--- dna.js
+++ dna.js
@@ -1,4 +1,4 @@
-// DNAjs Template Cloner ~~ v0.3.5
+// DNAjs Template Cloner ~~ v0.3.6
 // MIT/GPLv3 ~~ dnajs.org/license.html
 // Copyright (c) 2013-2015 Center Key Software and other contributors
 
@@ -117,7 +117,7 @@ var dna = {
       },
    info: function() {
       var names = Object.keys(dna.store.templates);
-      console.log('~~ DNAjs v0.3.5 ~~');
+      console.log('~~ DNAjs v0.3.6 ~~');
       console.log('templates:', names.length);
       console.log('names:', names);
       console.log('store:', dna.store.templates);
