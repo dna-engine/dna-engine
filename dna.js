4ed4cdda08f93d2b5540d7db125b86887db820ca
dpilafian <dem@centerkey.com>
2017-04-23 18:53:13 -0700

    Version number updated for next release

--- dna.js
+++ dna.js
@@ -1,4 +1,4 @@
-// dna.js Semantic Templates ~~ v1.2.7
+// dna.js Semantic Templates ~~ v1.2.8
 // MIT ~~ dnajs.org/license
 // Copyright (c) 2013-2017 individual contributors to dna.js
 
@@ -143,7 +143,7 @@ var dna = {
       var names = Object.keys(dna.store.templates);
       function addToSum(sum, name) { return sum + dna.store.templates[name].clones; }
       return {
-         version:      '1.2.7',
+         version:      '1.2.8',
          templates:    names.length,
          clones:       names.reduce(addToSum, 0),
          names:        names,
