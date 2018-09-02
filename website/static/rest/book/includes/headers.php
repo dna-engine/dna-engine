7300c64cb8a9e7a4820866d35a9527065739007b
Dem Pilafian <dem@centerkey.com>
2018-09-01 23:43:25 -0700

    Fix cors restriction with books rest api

--- website/static/rest/book/includes/headers.php
+++ website/static/rest/book/includes/headers.php
@@ -1,6 +1,6 @@
 <?php
-// dna.js
 $origin = isset($_SERVER["HTTP_ORIGIN"]) ? $_SERVER["HTTP_ORIGIN"] : "*";
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Origin: " . $origin);
 header("Content-Type: application/json");
 ?>
