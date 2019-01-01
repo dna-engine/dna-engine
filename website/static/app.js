781f566b78de1b47c48e2e56dcc2a10873921244
Dem Pilafian <dem@centerkey.com>
2019-01-01 02:38:48 -0800

    Happy new year

--- website/static/app.js
+++ website/static/app.js
@@ -1,5 +1,5 @@
 // dna.js Website ~~ dnajs.org ~~ MIT
-// Copyright (c) 2013-2018 individual contributors to dna.js
+// Copyright (c) 2013-2019 individual contributors to dna.js
 
 const app = {
    setup: () => {
