4a4e675fd076579651f7e2c0390d340ef6c87448
Katie O'Neill <katieoneillsMacBookAir@Katies-MacBook-Air.local>
2019-04-22 19:30:33 -0700

    updated focus

--- dna.js
+++ dna.js
@@ -283,7 +283,7 @@ dna.ui = {
       },
    focus: (elem) => {
       // Sets focus on an element.
-      return elem.focus();
+      return elem.trigger('focus');
       },
    getAttrs: (elem) => {
       // Returns the attributes of the DOM node in a regular array.
