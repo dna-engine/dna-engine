bc90d61b2553d12a73956b18e811414fad7f0ca8
Dem Pilafian <dem@centerkey.com>
2020-05-04 01:20:27 -0700

    Use lsof to detect if web server is already running

--- setup.sh
+++ setup.sh
@@ -21,18 +21,15 @@ setupTools() {
 
 setupWebServer() {
    cd $projectHome
-   port=$(grep web-server package.json | sed -e "s/[^0-9]//g")
-   # Requires package.json script: "web-server": "http-server --port 8080 &"
+   port=$(grep web-server package.json | sed 's/[^0-9]*\([0-9]*\).*/\1/')  #extract port number from script
    echo "Web Server (indexzero/http-server on node):"
-   # test -z "$(pgrep -f $projectHome)" && npm run web-server
-   test -z "$(pgrep -f http-server)" && npm run web-server
-   pgrep -fl http-server
+   npm run web-server
+   sleep 2  #ensure pid is ready to read
    echo "To stop web server:"
-   echo "   $ pgrep -fl http-server"
-   # echo "   $ pkill -f $projectHome"
-   echo "   $ pkill -f http-server"
+   echo "   $ lsof -P -i TCP:$port"
+   echo "   $ kill $(lsof -Pt -i TCP:$port)"
    echo
    }
 
 setupTools
-setupWebServer  #port: dna -> d9a -> 0xD9A -> 3482
+setupWebServer
