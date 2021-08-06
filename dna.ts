f87be6c5ef46798ae1e5b9247f20b44ecbb30364
dpilafian <dem@centerkey.com>
2021-08-06 14:07:43 -0700

    More forgiving json type declaration

--- dna.ts
+++ dna.ts
@@ -1,6 +1,6 @@
 // dna.js ~~ MIT License
 
-export type Json = string | number | boolean | null | Json[] | { [key: string]: Json };
+export type Json = string | number | boolean | null | undefined | Json[] | { [key: string]: Json };
 export type JsonObject = { [key: string]: Json };
 export type JsonArray = Json[];
 export type JsonData = JsonObject | JsonArray;
